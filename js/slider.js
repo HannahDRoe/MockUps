553829f3fc25a3f44613eb4f7cf80a24f3205c76
Hannah Roe <hannahdroe@gmail.com>
2016-06-30 19:07:36 -0600

    Finished slider, working on about section

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,24 @@
 // Initalize Slick Slider
   $(document).ready(function () {
         $(".top-slider").slick({
-          dots: true,
-          arrows: true,
-          
-        //   customPaging : function(slider, i) {
-        // var thumb = $(slider.$slides[i]).data('thumb');
-        // return '<a><img src="'+thumb+'"></a>';
-   // },
-
+          	dots: true,
+          	arrows: true,
+          	mobileFirst: true,
+          	infinite: false,
+          	draggable: false,
+          	prevArrow:"<img class='slick-prev' src='css/img/arrowPrev.svg'>",
+      		nextArrow:"<img class='slick-next' src='css/img/arrowNext.svg'>",
 	   		responsive: [{
 	   			breakpoint:1024,
 	   			settings: {
-	   				arrows: false
-	   			}
-	   		}]
+	   				arrows: true
+	   			},
+	   			 breakpoint: 600,
+	   			 settings:{
+	   			 	arrows: true
+	   			 }
+	   		}],
+	
         });
+
     })
