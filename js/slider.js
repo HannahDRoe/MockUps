f72ca72087a9ad93754cef99bb29de72a91fe60d
Hannah Roe <hannahdroe@gmail.com>
2016-07-06 18:29:58 -0600

    Finished team section. Started on works section. Removed accidental sass-cache temp files.

--- js/slider.js
+++ js/slider.js
@@ -6,8 +6,8 @@
           	mobileFirst: true,
           	infinite: false,
           	draggable: false,
-          	prevArrow:"<img class='slick-prev' src='css/img/arrowPrev.svg'>",
-      		nextArrow:"<img class='slick-next' src='css/img/arrowNext.svg'>",
+          	prevArrow:"<img class='slick-prev' src='css/img/whiteArrowPrev.svg'>",
+      		  nextArrow:"<img class='slick-next' src='css/img/whiteArrowNext.svg'>",
 	   		responsive: [{
 	   			breakpoint:1024,
 	   			settings: {
@@ -19,12 +19,29 @@
 	   			 }
 	   		}],
 	
-        });
+      });
         $(".team-slider").slick({
-    		dots: true,
-			arrows: true,
-            slidesToShow: 4
+          	dots: true,
+      			arrows: true,
+            slidesToShow: 4,
+            draggable: false,
+            prevArrow:"<img class='slick-prev' src='css/img/greyArrowPrev.svg'>",
+            nextArrow:"<img class='slick-next' src='css/img/greyArrowNext.svg'>",
+            responsive: [
+              {
+                breakpoint: 1000,
+                settings: {
+                  slidesToShow: 3
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2
+                }
+              }
 
-        });
+            ]
+      });
 
     })
