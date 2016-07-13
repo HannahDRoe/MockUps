ed5ce62c321364bdd40ae6c0947697d182b16099
Hannah Roe <hannahdroe@gmail.com>
2016-07-12 18:08:54 -0600

    Added functionality to Load More button in blog section

--- js/app.js
+++ js/app.js
@@ -9,4 +9,15 @@ $('.work-btns').click(function(event) {
 	$('.work-btns').removeClass('selected');
 	$(this).addClass('selected');
 });
+$('#load-btn').click(function(e){
+	e.preventDefault();
+	if ($('.hide-me').hasClass('hide')) {
+		$('.hide-me').removeClass('hide');
+		$('#load-btn').text('Read Less...');
+	}else {
+		$('.hide-me').addClass('hide');
+		$('#load-btn').text('Load More...')
+	}
 
+
+});
