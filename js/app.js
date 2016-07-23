05288f9db9557e694fed3742b17998a8d93d749f
Hannah Roe <hannahdroe@gmail.com>
2016-07-23 12:17:35 -0600

    Added Sticky and Magellan to navigation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
 $(document).foundation();
-$('.menu-btn').click(function(event) {
-	event.preventDefault();
-	$('.menu-btn').removeClass('active');
-	$(this).addClass('active');
-});
 $('.work-btns').click(function(event) {
 	event.preventDefault();
 	$('.work-btns').removeClass('selected');
